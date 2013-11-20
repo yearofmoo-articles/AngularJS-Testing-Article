2c53b4a6c8ec66cfb16ce3bf6bb7b1b1d8b9c4cd
Matias Niemelä <matias@yearofmoo.com>
2013-11-20 14:28:54 -0500

    add missing services midway test

--- test/midway/services/serviceSpec.js
+++ test/midway/services/serviceSpec.js
@@ -11,7 +11,22 @@ describe("Midway: Testing Services", function() {
     tester = ngMidwayTester('App');
   });
 
-  it('should have a working timer', function() {
+  it('should perform a JSONP operation to youtube and fetch data', function(done) {
+    var $yt = tester.inject('$appYoutubeSearcher');
+    expect($yt).not.to.equal(null);
+
+    //this is the first video ever uploaded on youtube
+    //so I doubt it will be removed anytime soon
+    //and should be a good testing item
+    var youtubeID = 'jNQXAC9IVRw';
+
+    $yt.findVideo(youtubeID, false,
+      function(q, data) {
+        expect(data).not.to.equal(null);
+        expect(data.id).to.equal(youtubeID);
+        done();
+      }
+    );
   });
 
 });
